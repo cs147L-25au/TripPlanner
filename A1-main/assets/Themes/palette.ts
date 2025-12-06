da7e81b3e96d4add5ac3d74e9a0f5689bf3b52f2
sohrabhassibi <sohrabh@stanford.edu>
2025-12-05 22:20:08 -0800

    delete

--- A1-main/assets/Themes/palette.ts
+++
@@ -1,8 +0,0 @@
-// Feel free to customize these to your liking or define new colors!
-export const palette = {
-  white: "#FFFFFF",
-  lightGray: "#F3F3F3",
-  black: "#000000",
-  lightBlack: "#121416",
-  lighterBlack: "#202224",
-};
